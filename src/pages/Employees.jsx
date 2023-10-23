4a32a25d50956d4226d3020b36258e684c847c2a
Dana Nhem <dionysiosnhem@gmail.com>
2023-10-23 09:20:29 +0200

    create reusable axios http requests functions

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { getEmployees } from "../api/employees";
+import { useState, useEffect } from "react";
 
 const Employees = () => {
+  const [employees, setEmployees] = useState(null);
+
+  useEffect(() => {
+    try {
+      async function fetchEmployees() {
+        const response = await getEmployees();
+        setEmployees(response.data.employees);
+      }
+      fetchEmployees();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  console.log(employees);
+
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>Employees</h1>
+      <ul>
+        {employees ? (
+          employees.map((e) => (
+            <li key={e._id} style={{ textAlign: "center" }}>
+              {e.name}
+            </li>
+          ))
+        ) : (
+          <h1 style={{ textAlign: "center" }}>Loading</h1>
+        )}
+      </ul>
     </div>
   );
 };
