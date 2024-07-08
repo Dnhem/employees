ffbf2b80a8d454d8297cc7327952ce4fb395c53c
Dana Nhem <dana@Danas-MacBook-Pro.local>
2024-07-08 19:48:15 +0200

    add raccoons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,7 +18,7 @@ export default function ClippedDrawer() {
       >
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-            Checkly Employees 🦝
+            Checkly Employees 🦝🦝
           </Typography>
         </Toolbar>
       </AppBar>
